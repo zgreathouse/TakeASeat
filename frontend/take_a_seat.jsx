74e5dce2e451ada6d4268694a4711d98fab7505e
zgreathouse <greathouse.zachary@gmail.com>
2017-09-28 13:55:56 -0700

    modified entry file for testing ajax requests

--- frontend/take_a_seat.jsx
+++ frontend/take_a_seat.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+//for testing
+import { signup, login, logout } from './util/session_api_util';
+//end testing
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
+
   const rootElement = document.getElementById('root')
   ReactDOM.render(<h1>Please, Take A Seat.</h1>, rootElement);
 });
