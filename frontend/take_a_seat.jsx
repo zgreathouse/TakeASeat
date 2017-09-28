0817df56f1780956c38a31650fb7b5ca3d9b4eba
zgreathouse <greathouse.zachary@gmail.com>
2017-09-27 18:25:10 -0700

    create user table and model, write users, sessions, and application controllers, write routes in route file under namespace api, add BCrypt gem for hashing passwords

---
+++ frontend/take_a_seat.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootElement = document.getElementById('root')
+  ReactDOM.render(<h1>Please, Take A Seat.</h1>, rootElement);
+});
