7cd8a54da313e6174152562bd62564384e00ffa4
zgreathouse <greathouse.zachary@gmail.com>
2017-09-28 18:08:20 -0700

    refactor session_actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,5 +1,6 @@
 import * as SessionAPIUtil from '../util/session_api_util';
 
+//constants
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
 
@@ -16,7 +17,6 @@ export const receiveErrors = errors => ({
 });
 
 //async actions
-
 export const signup = user => dispatch => (
   SessionAPIUtil.signup(user).then(user => (
     dispatch(receiveCurrentUser(user))
@@ -34,7 +34,7 @@ export const login = user => dispatch => (
 );
 
 export const logout = () => dispatch => (
-  SessionAPIUtil.logout().then(user => (
+  SessionAPIUtil.logout().then(() => (
     dispatch(receiveCurrentUser(null))
   ))
 );
